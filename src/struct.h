3ba43c9d36d9685a585c3ec4913a9698ed4daad8
Grisella Edgecombe <griselle@student.21-school.ru>
2022-03-23 11:23:15 +0300

    add struct.h

---
+++ src/struct.h
@@ -0,0 +1,27 @@
+// Copyright [2022] <griselle, sparelis, laynadre>
+
+#ifndef SRC_STRUCT_H_
+#define SRC_STRUCT_H_
+
+typedef struct tModules {
+    int id;
+    char module_name[30];
+    int mem_level_modules;
+    int cell_num;
+    int deletion_flag;
+} modules;
+
+typedef struct tLevels {
+    int mem_level_levels;
+    int cell_amount;
+    int protect_flag;
+} levels;
+
+typedef struct tStatus {
+    int event_id;
+    int module_id;
+    int new_status;
+    char status_change_date[11];
+    char status_change_time[9];
+} status_events;
+#endif  // SRC_STRUCT_H_
