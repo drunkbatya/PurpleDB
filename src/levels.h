056a7ac1eafd4f8b36f4cd753287e380f61469d4
Spare Lisa <sparelis@student.21-school.ru>
2022-03-24 14:45:04 +0300

    test levels functions

---
+++ src/levels.h
@@ -0,0 +1,23 @@
+#ifndef SRC_MODULES_H_
+#define SRC_MODULES_H_
+
+typedef struct tLevels {
+    int mem_level_levels;
+    int cell_amount;
+    int protect_flag;
+} levels;
+
+void select_for_levels(char **field, char **where);
+void print_mask_levels(int identifier);
+int compare_levels (levels *local, int check_field, char *temp);
+int get_records_count_in_file(FILE *pfile);
+levels read_record_from_file(FILE *pfile, int index);
+void print_struct_levels(levels *local, int identifier);
+void insert_for_levels(char **new_line);
+void write_record_in_file(FILE *pfile, modules *record_to_write, int index);
+int get_file_size_in_bytes(FILE *pfile);
+void update_for_levels(char **old, char **new);
+void update_record_levels(FILE *pfile, modules *local, modules *change, int index);
+int compare_for_update_levels (modules *local, modules *where);
+
+#endif  // SRC_MODULES_H_
--- src/modules.h
+++ src/modules.h
@@ -1,5 +1,5 @@
-#ifndef SRC_FUNCTIONS_H_
-#define SRC_FUNCTIONS_H_
+#ifndef SRC_MODULES_H_
+#define SRC_MODULES_H_
 
 typedef struct tModules {
     int id;
@@ -8,20 +8,6 @@ typedef struct tModules {
     int cell_num;
     int deletion_flag;
 } modules;
-
-typedef struct tLevels {
-    int mem_level_levels;
-    int cell_amount;
-    int protect_flag;
-} levels;
-
-typedef struct tStatus {
-    int event_id;
-    int module_id;
-    int new_status;
-    char status_change_date[11];
-    char status_change_time[9];
-} status_events;
 
 void select_for_modules(char **field, char **where);
 int compare (modules *local, int check_field, char * temp);
@@ -37,4 +23,4 @@ void update_record(FILE *pfile, modules *local, modules *change, int index);
 int compare_for_update (modules *local, modules *where);
 void print_mask(int identifier);
 
-#endif  // SRC_FUNCTIONS_H_
+#endif  // SRC_MODULES_H_
