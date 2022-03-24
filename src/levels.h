7f0122de4b18efb3d74b7134fb6d48948089c06c
Grisella Edgecombe <griselle@student.21-school.ru>
2022-03-24 19:38:32 +0300

    make levels great again

--- src/levels.h
+++ src/levels.h
@@ -7,17 +7,18 @@
 
 void select_for_levels(char **field, char **where);
 void print_mask_levels(int identifier);
-int compare_levels (levels *local, int check_field, char *temp);
+int compare_levels(levels *local, int check_field, char *temp);
 int get_records_count_in_file_levels(FILE *pfile);
 levels read_record_from_file_levels(FILE *pfile, int index);
 int get_file_size_in_bytes_levels(FILE *pfile);
 void print_struct_levels(levels *local, int identifier);
 void insert_for_levels(char **new_line);
-void write_record_in_file_levels (FILE *pfile, levels *record_to_write, int index);
+void write_record_in_file_levels(FILE *pfile, levels *record_to_write, int index);
 void update_for_levels(char **old, char **new);
 void update_record_levels(FILE *pfile, levels *local, levels *change, int index);
 int compare_for_update_levels(levels *local, levels *where);
 int get_records_count_in_file_levels(FILE *pfile);
+void print_outro_levels(int identifier);
 
 
 #endif  // SRC_LEVELS_H_
