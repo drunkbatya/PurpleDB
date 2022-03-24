9c118585fe0ef4705c729fd50e27692edc3727d9
Grisella Edgecombe <griselle@student.21-school.ru>
2022-03-24 17:00:50 +0300

    cpplint fix

--- src/levels.c
+++ src/levels.c
@@ -1,3 +1,5 @@
+// Copyright [2022] <griselle, sparelis, laynadre>
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
--- src/levels.h
+++ src/levels.h
@@ -1,10 +1,13 @@
+// Copyright [2022] <griselle, sparelis, laynadre>
+
 #ifndef SRC_LEVELS_H_
 #define SRC_LEVELS_H_
+
 #include "main.h"
 
 void select_for_levels(char **field, char **where);
 void print_mask_levels(int identifier);
-int compare_levels (levels *local, int check_field, char *temp);
+int compare_levels(levels *local, int check_field, char *temp);
 int get_records_count_in_file(FILE *pfile);
 levels read_record_from_file_levels(FILE *pfile, int index);
 void print_struct_levels(levels *local, int identifier);
@@ -13,7 +16,7 @@ void write_record_in_file_levels(FILE *pfile, levels *record_to_write, int index
 int get_file_size_in_bytes(FILE *pfile);
 void update_for_levels(char **old, char **new);
 void update_record_levels(FILE *pfile, levels *local, levels *change, int index);
-int compare_for_update_levels (levels *local, levels *where);
+int compare_for_update_levels(levels *local, levels *where);
 void print_outro_levels(int identifier);
 
 #endif  // SRC_LEVELS_H_
