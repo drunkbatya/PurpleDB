2184fcf93391c7d92f323c1844c5928b858197b6
Layna Drew <laynadre@vo-j1.msk.21-school.ru>
2022-03-24 16:58:23 +0300

    fixes

--- src/binary.c
+++ src/binary.c
@@ -14,8 +14,8 @@ int get_records_count_in_file_modules(FILE *pfile) {
 int get_file_size_in_bytes(FILE *pfile) {
     int size = 0;
     fseek(pfile, 0, SEEK_END);    // Move the position pointer to the end of the file.
-    size = ftell(pfile);          // Read the number of bytes from the beginning of the file to the current position pointer.
-    rewind(pfile);                // For safety reasons, move the position pointer back to the beginning of the file.
+    size = ftell(pfile);          // Read to the current position pointer.
+    rewind(pfile);                // For n pointer back to the beginning of the file.
     return size;
 }
 
